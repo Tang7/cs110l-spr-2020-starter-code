8c7ced23298a94cdc0a754a9ce471eae29162d84
Tang7 <still.tangqi@gmail.com>
2023-12-29 23:37:14 +0200

    Complete mini diff

--- week2/rdiff/src/grid.rs
+++ week2/rdiff/src/grid.rs
@@ -22,6 +22,14 @@ impl Grid {
         (self.num_rows, self.num_cols)
     }
 
+    fn get_index(&self, row: usize, col: usize) -> Option<usize> {
+        if row < self.num_rows && col < self.num_cols {
+            Some(row * self.num_cols + col)
+        } else {
+            None
+        }
+    }
+
     /// Returns the element at the specified location. If the location is out of bounds, returns
     /// None.
     ///
@@ -30,18 +38,23 @@ impl Grid {
     /// but others argue that makes code needlessly complex. Here, we decided to return Option to
     /// give you more practice with Option :) and because this similar library returns Option:
     /// https://docs.rs/array2d/0.2.1/array2d/struct.Array2D.html
-    #[allow(unused)] // TODO: delete this line when you implement this function
     pub fn get(&self, row: usize, col: usize) -> Option<usize> {
-        unimplemented!();
-        // Be sure to delete the #[allow(unused)] line above
+        self.get_index(row, col).map(|index| self.elems[index])
+    }
+
+    fn get_mut(&mut self, row: usize, col: usize) -> Option<&mut usize> {
+        self.get_index(row, col)
+            .map(move |index| &mut self.elems[index])
     }
 
     /// Sets the element at the specified location to the specified value. If the location is out
     /// of bounds, returns Err with an error message.
-    #[allow(unused)] // TODO: delete this line when you implement this function
     pub fn set(&mut self, row: usize, col: usize, val: usize) -> Result<(), &'static str> {
-        unimplemented!();
-        // Be sure to delete the #[allow(unused)] line above
+        self.get_mut(row, col)
+            .map(|location| {
+                *location = val;
+            })
+            .ok_or("location is out of bound")
     }
 
     /// Prints a visual representation of the grid. You can use this for debugging.
